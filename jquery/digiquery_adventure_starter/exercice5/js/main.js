76a82c639338acb0359222cce2eecc17f4b78573
rbelounis <65893991+r-belounis@users.noreply.github.com>
2020-06-30 12:10:58 +0200

    Reworked folders for better organisation

--- jquery/digiquery_adventure_starter/exercice5/js/main.js
+++ jquery/digiquery_adventure_starter/exercice5/js/main.js
@@ -1,5 +1,4 @@
 var heading1El = document.querySelector("h1");
-var heading2El = document.querySelector("h2");
 
 heading1El.style.color = "blue";
 heading1El.style.fontSize = "2em";
--- node_params/index.js
+++
@@ -1,7 +0,0 @@
-function sayHello(name){
-    console.log('Nice to meet you' + name);
-}
-
-sayHello('to my friend');
-
-console.log(process.argv[2]);
