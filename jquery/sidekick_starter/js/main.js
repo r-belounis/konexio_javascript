55d7eca13b69aaefb8e66c0f9eb9d90da5740364
rbelounis <65893991+r-belounis@users.noreply.github.com>
2020-07-05 23:55:36 +0200

    almost cleared exercice, miss the icons

--- jquery/sidekick_starter/js/main.js
+++ jquery/sidekick_starter/js/main.js
@@ -1,44 +1,20 @@
+// Waypoint + CounterUp
 jQuery(document).ready(function($) {
 
-    // 1) Animation text
-    // -----------------
-    var textAnimation = $('#animation-text').animate({left: "500"});
-    var textAnimation2 = $('#animation-text2').animate({left: "500"}, 1000);
-
-    // Waypoints + animation on scroll
-    // var textAnimationOnScroll = $('#animation-scroll').waypoints({
-    //     handler: function(direction) {
-    //         if (direction == "up") {
-    //             textAnimation.reset();
-    //             textAnimation2.reset();
-    //           } else {
-    //             textAnimation.start();
-    //             textAnimation2.start();
-    //           }
-    //         },
-        // offset: '50%'
-    // });
-
-
-    // 2) Pricing
-    // ----------
     // CounterUp on pricing classes
     var counterUp = $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
 
-    // Waypoints + counterUp on scroll
-    // var countOnScroll = $('#pricing').waypoint({
-    //     handler: function(direction) {
-    //       if (direction == "up") {
-    //         counterUp.reset();
-    //       } else {
-    //         counterUp.start();
-    //       }
-    //     },
-        // offset: '50%'
-    // });
-
 // jQuery End
+});
+
+// WOW animations
+new WOW().init();
+
+// Smooth scrolling
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+	speedAsDuration: true
 });
